2691fc000596777b317f762fa4dd5785c64dcf9b
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-04-30 09:04:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 // 6kyu 
 function longestRepetition(s) {
-  const lowercaseS = s.toLowerCase();
-  let lastChar = ''; 
-  let lastCount = 0; 
-  let result = [lastChar, lastCount];
+  const lowercaseS = s.toLowerCase()
+  let lastChar = ''
+  let lastCount = 0 
+  let result = [lastChar, lastCount]
   for (const char of lowercaseS) {
     if (char == lastChar) {
-      lastCount++;
+      lastCount++
     } else {
-      lastChar = char;
-      lastCount = 1;
+      lastChar = char
+      lastCount = 1
     }
     
     if (result[1] < lastCount) {
-      result = [lastChar, lastCount];
+      result = [lastChar, lastCount]
     }
   }
-  return result;
+  return result
 }
 
-console.log(longestRepetition('aaaabb'));
-console.log(longestRepetition('dabcDddd'));
+console.log(longestRepetition('aaaabb'))
+console.log(longestRepetition('dabcDddd'))
 
 // 7
 function validateWord(s) {
-  s = s.toLowerCase();
-  return s.length % new Set(s).size == 0;
+  s = s.toLowerCase()
+  return s.length % new Set(s).size == 0
 }
 
-console.log(validateWord("abcabc"));
-console.log(validateWord("abc:abc"));
+console.log(validateWord("abcabc"))
+console.log(validateWord("abc:abc"))
 
 // 7
 function removeConsecutiveDuplicates(string) {
-  let arr = string.toLowerCase().split(' ');
-  let result = [];
+  let arr = string.toLowerCase().split(' ')
+  let result = []
   for (let i = 0; i < arr.length; i++) {
-    let word = arr[i];
+    let word = arr[i]
     if (!(word === arr[i + 1])) {
-      result.push(word);
+      result.push(word)
     }
   }
-  return result.join(' ');
+  return result.join(' ')
 }
 
-console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
-console.log(removeConsecutiveDuplicates("alpha alpha alpha alpha"));
+console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))
+console.log(removeConsecutiveDuplicates("alpha alpha alpha alpha"))
 
 // 6kyu
 function toCamelCase(str){
