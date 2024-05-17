7c4b314cc3c639a6cfb0a9e67ee37a1ae912ff21
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-05-17 12:23:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#    is_even(int)
 print('\n is_even')
 def is_even(i):   
     if i % 2 != 0 :
@@ -8,7 +9,7 @@ def is_even(i):
 print(is_even(3))
 print(is_even(8))
 
-
+#    div_by(num, divisor)
 print('\n div_by')
 def div_by(n, d):
         if n > 0 and d > 0 :
@@ -24,14 +25,15 @@ print(div_by(0,3))     # print False
 print(div_by(195,13))   # returns True
 
 
-
+#    is_even_range()
 print('\n is_even_range')
-def is_even_range() :
-    for i in range(1, 11) :
+def is_even_range(n1, n2) :
+    for i in range(n1, n2+1) :
             if i % 2 == 0 :
                 print('even', i)
             else :
                 print('odd', i)
 
-is_even_range()
+print(is_even_range(1, 10))
+print(is_even_range(2, 12))
 
